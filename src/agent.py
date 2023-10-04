338897eda71ffabe3f089ad18a9ea479c29e5aac
Yuji Cao <travisyjcao@gmail.com>
2023-10-04 15:08:34 +0800

    rule agent implemented

--- src/agent.py
+++ src/agent.py
@@ -1,9 +1,21 @@
+import numpy as np
+
+
 class RuleAgent:
-    def __init__(self):
+    def __init__(self, gen_id):
         # Initialize any necessary variables or data structures
         pass
 
-    def act(self, prev_carbon_emission,):
+    def act(self, prev_emission, allowance, remaining_time):
         # Implement the agent's decision-making logic based on the observation
-        # Return the agent's action
 
+        prev_emission_total = np.sum(prev_emission)
+        prev_emission_mean = np.mean(prev_emission)
+        remaining_allowance = allowance - prev_emission_total
+        emission_expected = prev_emission_mean * remaining_time
+
+        # if action > 0, buy extra allowance, else sell allowance
+        action = emission_expected - remaining_allowance
+
+        # return (buy, sell) pair
+        return (max(action, 0), max(-action, 0))
